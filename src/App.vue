2a4b57b134a2b403b98515450ab043c4fdc664fa
Geovane Dias de Araujo <Diasgeovane7@gmail.com>
2024-12-06 16:30:59 -0300

    Adicionado o componente de Contagem

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import Contagem from "./components/Contagem.vue"
 import Intro from "./components/Intro.vue"
 </script>
 
 <template>
   <Intro/>
+  <Contagem/>
 </template>
 
 <style scoped>
